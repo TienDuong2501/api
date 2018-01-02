1f2b368e5e1c5c780e2ff953f719f551a6ead5c9
DuongNT <tienduong250196@gmail.com>
2018-01-02 14:50:55 +0700

    ProductCollection Resource

--- routes/api.php
+++ routes/api.php
@@ -14,3 +14,5 @@ Route::apiResource('/products','ProductController');
 Route::group(['prefix' => 'products'],function(){
 	Route::apiResource('/{product}/reviews','ReviewController');
 });
+//day la duong dan url de truy cap vao apiroute
+// http://127.0.0.1:8000/api/products/25
